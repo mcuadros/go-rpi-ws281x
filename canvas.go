2dd7f3d8fcfaca9e6da7413e404e49ee0cded10d
Máximo Cuadros <mcuadros@gmail.com>
2016-10-16 22:53:24 +0200

    canvas tests

--- canvas.go
+++ canvas.go
@@ -6,12 +6,16 @@ import (
 	"image/draw"
 )
 
+// Canvas is a image.Image representation of a WS281x matrix, it implements
+// image.Image interface and can be used with draw.Draw for example
 type Canvas struct {
 	w, h   int
 	m      Matrix
 	closed bool
 }
 
+// NewCanvas returns a new Canvas using the given width and height and creates
+// a new WS281x matrix using the given config
 func NewCanvas(w, h int, config *HardwareConfig) (*Canvas, error) {
 	m, err := NewWS281x(w*h, config)
 	if err != nil {
@@ -35,11 +39,12 @@ func (c *Canvas) Render() error {
 	return c.m.Render()
 }
 
+// ColorModel returns the canvas' color model, always color.RGBAModel
 func (c *Canvas) ColorModel() color.Model {
 	return color.RGBAModel
 }
 
-// Bounds return the topology of the Matrix
+// Bounds return the topology of the Canvas
 func (c *Canvas) Bounds() image.Rectangle {
 	return image.Rect(0, 0, c.w, c.h)
 }
@@ -59,14 +64,15 @@ func (c *Canvas) position(x, y int) int {
 	return x + (y * c.w)
 }
 
+// Clear set all the leds on the matrix with color.Black
 func (c *Canvas) Clear() error {
 	draw.Draw(c, c.Bounds(), &image.Uniform{color.Black}, image.ZP, draw.Src)
 	return c.m.Render()
 }
 
+// Close clears the matrix and close the matrix
 func (c *Canvas) Close() error {
 	c.Clear()
-
 	return c.m.Close()
 }
 
